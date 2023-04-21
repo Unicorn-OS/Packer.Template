ddd41ba0ff6c3ba8f4ef8b4c3faf2fdbdd2dc7b8
Dark Star☀🎵 <darkstar@unisocial.net>
2023-04-21 16:17:07 -0400

    debian virtualbox works

---
+++ Chef Bento/works:/run.sh
@@ -0,0 +1,20 @@
+# Install packer then:
+# https://github.com/chef/bento#using-packer
+
+clone(){
+    git clone https://github.com/chef/bento.git
+    cd bent/
+}
+
+install_builders(){
+    packer init -upgrade ./packer_templates
+}
+
+test(){
+    # worked on: 4/21/2023 chef/bento commit b8eca156fc9c29a42baceb7f3c830bfb9a34a769
+
+    # VirtualBox
+    ## Debian
+    ### x86_64
+    packer build -only=virtualbox-iso.vm -var-file=os_pkrvars/debian/debian-11-x86_64.pkrvars.hcl ./packer_templates
+}
