ae718819f37db762797a864ab24df07554b70d6c
🦄Pegasus (DarkStar☀) <darkstar@unisocial.net>
2023-06-14 02:19:31 -0400

    refactor

--- Chef Bento/test/run.sh
+++ Chef Bento/test/run.sh
@@ -3,18 +3,22 @@
 
 clone(){
     git clone https://github.com/chef/bento.git
-    cd bent/
+    cd bento
 }
 
 install_builders(){
     packer init -upgrade ./packer_templates
 }
 
-test(){
-    # Debian
-    # VirtualBox
+test_virtualbox(){
     ## worked on: chef/bento commit b8eca156fc9c29a42baceb7f3c830bfb9a34a769
-    packer build -only=virtualbox-iso.vm -var-file=os_pkrvars/debian/debian-11-x86_64.pkrvars.hcl ./packer_templates
+    packer build --only=virtualbox-iso.vm -var-file=os_pkrvars/debian/debian-11-x86_64.pkrvars.hcl ./packer_templates
+}
+
+test_qemu(){
+    packer build --only=qemu.vm -var-file=os_pkrvars/ubuntu/ubuntu-22.04-x86_64.pkrvars.hcl ./packer_templates
+    packer build --only=qemu.vm -var-file=os_pkrvars/debian/debian-11-x86_64.pkrvars.hcl ./packer_templates
+    packer build --only=qemu.vm -var-file=os_pkrvars/ubuntu/ubuntu-22.04-x86_64.pkrvars.hcl ./packer_templates
 
 }
 
@@ -24,7 +28,5 @@ patch(){
     ## Problem: Fails at final step: Running "grub-install"
     ## Cause: seems to be trying to install Grub on disk 'sda' where only '/dev/vda' is available.
     ## Solution: Patch 'bento/packer_templates/http/debian/preseed.cfg' by changing 'sda' to 'vda'!
-
-    ## run:
-    shell: "packer build --only=qemu.vm -var-file=os_pkrvars/debian/debian-11-x86_64.pkrvars.hcl ./packer_templates"
+    echo "patch in ansible"
 }
