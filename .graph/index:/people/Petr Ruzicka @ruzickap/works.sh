6532ebeb9a119697155ad39982934729669deff0
🦄Pegasus (DarkStar☀) <darkstar@unisocial.net>
2023-08-17 19:06:04 -0500

    Windows Server 2022 works!

--- .graph/index:/people/Petr Ruzicka @ruzickap/works.sh
+++ .graph/index:/people/Petr Ruzicka @ruzickap/works.sh
@@ -17,6 +17,11 @@ works(){
 ./build.sh ubuntu-20.04-server-amd64-libvirt
 
 # Windows 10 Desktop
+
+# Windows Server 2022
+# works! Takes well over an hour; most of the work is in ansible.
+# use: Vagrantfile-windows.template to create_vm()!
+./build.sh windows-server-2022-standard-x64-eval-libvirt
 }
 
 main(){
